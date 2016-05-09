fd770ed8af733c4185ccd251710ba1d6fd0ae565
Milo Minderbinder <minderbinder.enterprises@gmail.com>
2016-05-09 17:52:33 -0400

    implmented basic puml generation configuration options and auto-creation of macros

--- src/awspuml.py
+++ src/awspuml.py
@@ -1,13 +1,14 @@
 import os
 import os.path
 import shutil
+import configparser
 import argparse
 import subprocess
 from itertools import groupby
-import re
 
 
 SRC_DIR = os.path.realpath(os.path.dirname(__file__))
+CONFIG_FILE = os.path.join(SRC_DIR, 'awspuml.conf')
 PUML_JAR = os.path.join(SRC_DIR, 'plantuml.jar')
 
 
@@ -24,19 +25,44 @@ def make_sprite(path, size='16'):
     return subprocess.check_output(cmd, universal_newlines=True)
 
 
-def create_puml(icon_path):
-    transparency = []
+def create_puml(icon_path, options=None):
+    if not options:
+        options = {}
+    icon_name = os.path.splitext(os.path.basename(icon_path))[0]
+    output = []
     sprite = make_sprite(icon_path)
     lines = sprite.split('\n')
-    transparency.extend(lines[:1])
+    output.extend(lines[:1])
     for line in lines[1:-3]:
-        transparency.append(line.replace('F', '0'))
-    transparency.extend(lines[-3:])
+        output.append(line.replace('F', '0'))
+    output.extend(lines[-3:])
+
+    macro_name = icon_name.upper()
+    entity_type = options.get('entity_type', 'component')
+    color = options.get('color', 'black')
+    stereotype = icon_name.replace('_', ' ').title()
+
+    output.append('!define %s(alias) AWS_ENTITY(%s,%s,%s,alias,%s)\n' % (
+        macro_name,
+        entity_type,
+        color,
+        icon_name,
+        stereotype
+    ))
+    output.append(
+        '!define %s(alias,label) AWS_ENTITY(%s,%s,%s,label,alias,%s)\n' % (
+            macro_name,
+            entity_type,
+            color,
+            icon_name,
+            stereotype
+        ))
+
     puml_file = '%s.puml' % os.path.splitext(icon_path)[0]
     print('Writing %s' % puml_file)
     with open(puml_file, 'w') as f:
         f.write('@startuml\n')
-        for line in transparency:
+        for line in output:
             f.write('%s\n' % line)
         f.write('@enduml\n')
 
@@ -67,7 +93,19 @@ def rename_duplicates(icon_paths):
             yield new_paths[0]
 
 
-def create_pumls(src, dest, ext='.png', sep='_'):
+def get_parent_dir(path, rel_path=None):
+    if rel_path:
+        path = os.path.relpath(path, rel_path)
+    dirname, basename = os.path.split(path)
+    if not basename:
+        return path
+    elif not dirname:
+        return basename
+    else:
+        return get_parent_dir(dirname)
+
+
+def create_pumls(src, dest, conf, ext='.png', sep='_'):
     src = os.path.realpath(src)
     dest = os.path.realpath(dest)
     icon_paths = [(op, create_structured_path(op, dest, sep)) for op in
@@ -80,13 +118,19 @@ def create_pumls(src, dest, ext='.png', sep='_'):
             os.makedirs(dest_dir)
         print('Copying icon to: %s' % icon_dest)
         shutil.copy2(icon_src, icon_dest)
-        create_puml(icon_dest)
+        category = get_parent_dir(icon_dest, rel_path=dest)
+        options = conf[category]
+        create_puml(icon_dest, options)
+    shutil.copy2(os.path.join(SRC_DIR, 'common.puml'), dest)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Generate PlantUML Sprites from AWS Simple Icons',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--config',
+                        default=os.path.join(SRC_DIR, 'awspuml.conf'),
+                        help='Config file for puml generation')
     parser.add_argument('icons_path',
                         help='Path to AWS Simple Icons directory')
     args = parser.parse_args()
@@ -95,6 +139,9 @@ if __name__ == '__main__':
     if not os.path.isdir(icons_path):
         raise Exception('Invalid path: %s' % icons_path)
 
+    config = configparser.ConfigParser()
+    config.read(args.config)
+
     output_path = os.path.join(SRC_DIR, 'output', 'AWS_Simple_Icons')
     print('Writing output to: %s' % output_path)
-    create_pumls(icons_path, output_path)
+    create_pumls(icons_path, output_path, config)
